07f746ac946d8e674239d8b9a57f33e16c0860e1
Jjingo Kisakye <sseguku@gmail.com>
2019-04-26 21:36:50 +0300

    react components

--- React/counterapp/src/components/counter.jsx
+++ React/counterapp/src/components/counter.jsx
@@ -5,7 +5,7 @@ class Counter extends Component {
     state = {
         count: 0,
         // imageURL : "https://picsum.photos/200"
-
+        tags: ['tag1','tag2','tag3']
     };
 
     styles ={
@@ -13,20 +13,33 @@ class Counter extends Component {
         fontWeigt: 'bold'
     };
     render() { 
+
+       
         return (
             //helps to avoid adding unnessary divs
-            <React.Fragment>
+            <div>
                 {/* <h1> Hello Counter Cart</h1> */}
                 {/* //setting attributes */}
                 {/* <img src = {this.state.imageURL} alt = ""/> */}
                 {/* <span style={{fontSize: 30}} className="badge badge-primary m-2"> {this.formatCount()}</span> */}
-                <span style={this.styles} className="badge badge-primary m-2"> {this.formatCount()}</span>
+                {/* <span style={this.styles} className={this.badgeDesign()}> {this.formatCount()}</span> */}
+                <span className={this.badgeDesign()}> {this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
-            </React.Fragment>
+                <ul>
+                    {this.state.tags.map(tag => <li key ={tag}>{tag}</li>)}
+                </ul>
+            </div>
         );
     }
 
+    badgeDesign() {
+        let countingDesign = "badge m-2 badge-";
+        countingDesign += this.state.count === 0 ? "warning" : "primary";
+        return countingDesign;
+    }
+
     formatCount(){
+       
         const { count } = this.state;
         return count === 0 ? 'Zero' : count;
     }
